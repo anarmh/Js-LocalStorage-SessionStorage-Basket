340d555779a09f3a517509903d27c2ce8f1af59e
anarmh <anarmh@code.edu.az>
2023-03-09 09:34:15 -0800

    add remove button

--- Basket/assets/js/cart.js
+++ Basket/assets/js/cart.js
@@ -2,38 +2,68 @@
 
 let products = JSON.parse(localStorage.getItem("basket"));
 let tableBody = document.querySelector("tbody");
-let productalert=document.querySelector(".alert-warning ")
+let productalert = document.querySelector(".alert-warning ");
 
-
-if(products!=null){
-    products.forEach((product) => {
-        tableBody.innerHTML += `
-      <tr>
+if (products != null) {
+  products.forEach((product) => {
+    tableBody.innerHTML += `
+      <tr data-id="${product.id}">
           <td><img src="${product.image}" alt=""></td>
            <td>${product.name}</td>
-              <td>${product.description}</td>
+          <td>${product.description}</td>
            <td>  <div class="count">
            <button class="decrease">-</button>
-           <input type="number" value="1" min="1" disabled>
+           <input type="text" class="product-quantity"  min="1" value="1" disabled >
            <button class="increase">+</button>
             </div>
            </td>
+           <td class="price-product">${product.price}</td>
+           <td> <button class="delete-item">Remove</button></td>
       </tr>`;
-      });
+  });
+
+  let decreaseBtns = document.querySelectorAll(".decrease");
+  let increaseBtns = document.querySelectorAll(".increase");
+  
+  
+  increaseBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let quantityInput = document.querySelector(".product-quantity");
+      let priceCell = document.querySelector(".price-product")
+      let quantityValue = parseInt(quantityInput.value) + 1;
+      quantityInput.value = quantityValue;
+      let total = parseInt(priceCell) * quantityValue;
+      priceCell.innerText = total;
+    });
+  });
+
+  removeProduct();
+} else {
+  tableBody.parentNode.classList.add("d-none");
+  productalert.classList.remove("d-none");
 }
-else{
-    tableBody.parentNode.classList.add("d-none");
-    productalert.classList.remove("d-none")
+
+function getBasketCount(arr) {
+  let count = 0;
+  for (const item of arr) {
+    count += item.count;
+  }
+  document.querySelector("sup").innerText = count;
 }
+getBasketCount(products);
 
+function removeProduct() {
+  let productsDelete = document.querySelectorAll(".delete-item");
 
-function getBasketCount(arr){
-
-    let count=0;
-    for (const item of arr) {
-        count+=item.count;
-    }
-    document.querySelector("sup").innerText=count;
+  productsDelete.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let deleteItem = this.closest("tr");
+      let deletedItemId = deleteItem.getAttribute("data-id");
+      deleteItem.remove();
+      let itemStorageId = products.findIndex((el) => el.id == deletedItemId);
+      products.splice(itemStorageId, 1);
+      localStorage.setItem("basket", JSON.stringify(products));
+      getBasketCount(products);
+    });
+  });
 }
-getBasketCount(products)
-
